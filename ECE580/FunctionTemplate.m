779fdb8943ce647058c644c63f796a68e7395af1
Joshua McCready <jmccread@umich.edu>
2018-05-29 08:15:19 -0400

    First commit, added some template files for consistency

---
+++ ECE580/FunctionTemplate.m
@@ -0,0 +1,43 @@
+function [output1,output2] = function_name(input1,input2,input3)
+%FUNCTION_NAME - One line description of what the function or script performs (H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%Optional file header info (to give more details about the function than in the H1 line)
+%
+% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+%
+% Inputs:
+%    input1 - Description
+%    input2 - Description
+%    input3 - Description
+%
+% Outputs:
+%    output1 - Description
+%    output2 - Description
+%
+% Example: 
+%    Line 1 of example
+%    Line 2 of example
+%    Line 3 of example
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+
+% Class:            ECE 580, Digital Signal Processing 
+% Assignment:       <Assignment>
+% Author:           Jonathan Gohl, Electrical Engineering M.S.E. Student
+% University of Michigan Dearborn, Electrical and Computer Engineering
+% email address:    jrgohl@umich.edu
+% Author:           Joshua McCready, Electrical Engineering M.S.E. Student
+% University of Michigan Dearborn, Electrical and Computer Engineering
+% email address:    jmccread@umich.edu
+% Month Year; Last revision: XX-Month-Year
+
+%------------- BEGIN CODE --------------
+
+%Enter your executable matlab commands here
+
+%------------- END OF CODE --------------
---
+++ ECE580/prob1_jmccread.asv
@@ -0,0 +1,7 @@
+% Author: Jonathan Gohl, Electrical Engineering M.S.E. Student
+% University of Michigan Dearborn, Electrical and Computer Engineering
+% email address: jrgohl@umich.edu
+% Author: Joshua McCready, Electrical Engineering M.S.E. Student
+% University of Michigan Dearborn, Electrical and Computer Engineering
+% email address: jmccread@umich.edu
+% Month Year; Last revision: XX-Month-Year
